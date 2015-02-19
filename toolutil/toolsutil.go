c922fa54ea90ce61999dd5353956a366d1fe741c
Mateus Braga <mateus.a.braga@gmail.com>
2015-02-18 22:06:15 -0500

    Add toolutil

---
+++ toolutil/toolsutil.go
@@ -0,0 +1,22 @@
+package toolutil
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+func verboseCmdRun(cmd *exec.Cmd) error {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	if err := cmd.Run(); err != nil {
+		log.Println("cmd:", cmd)
+		log.Printf("stdout:\n---Start---\n%v\n---End---", stdout.String())
+		log.Printf("stderr:\n---Start---\n%v\n---End---", stderr.String())
+		return fmt.Errorf("cmd '%v': %v", cmd, err)
+	}
+	return nil
+}
